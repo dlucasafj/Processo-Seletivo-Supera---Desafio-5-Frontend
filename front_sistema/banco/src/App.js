c344e24c61260466a7f0c2f919af470f7256fdc9
Daniel Lucas <daniel.lucasafj@gmail.com>
2022-03-27 23:00:55 -0300

    first commit

---
+++ front_sistema/banco/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { ExtratoScreen } from './page/ExtratoScreen';
+
+function App() {
+  return (
+    <div className="App">
+     <ExtratoScreen/>
+    </div>
+  );
+}
+
+export default App;
